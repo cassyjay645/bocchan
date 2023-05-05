5528ceb9e4cb129be73058adfe74705c1d7548b4
jomiller891 <128691552+jomiller891@users.noreply.github.com>
2023-05-05 06:45:12 +0200

    Update jad.java

--- jad.java
+++ jad.java
@@ -1,4 +1,7 @@
 
+import java.net.*;
+import java.io.*;
+
 public class jad{
     
     public static void main(String[] args) {
@@ -11,7 +14,31 @@ public class jad{
     
     public void exp(String url){
         log(url);
-    }
+        try
+        {
+            HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+            
+            InputStream ins = con.getInputStream();
+            
+            int red;
+            byte[] buf = new byte[2048*3];
+            
+            FileOutputStream o = new FileOutputStream("cat.jpg");
+            
+            while( (red = ins.read(buf)) != -1){
+                o.write(buf,0,red);
+            }
+            
+            o.flush();
+            o.close();
+            
+            
+            ins.close();
+            con.disconnect();
+        }
+        catch (IOException e)
+        {}
+    } 
     
     void log(String msg){
         System.out.println(msg);
